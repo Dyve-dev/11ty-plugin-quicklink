3d1cb57a79c9462ef1c95a04fe144d1a207c990c
igorovic <758832+igorovic@users.noreply.github.com>
2021-02-06 12:19:53 +0100

    refact and write tests

--- plugin/plugin.d.ts
+++ plugin/plugin.d.ts
@@ -1,4 +1,12 @@
 import { PluginOptions } from './types';
+export declare class Plugin {
+    private _options;
+    private _quicklinkUMD;
+    constructor(options?: PluginOptions);
+    quickLinkInit(options: any): string;
+    quicklinkUMD(): string;
+    quickLinkScript(): string;
+}
 export declare const plugin: {
     initArguments: {};
     configFunction: (eleventyConfig: any, options?: PluginOptions | undefined) => Promise<void>;
